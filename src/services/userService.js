ceac2c9130f4ceed760a952918f510f2dce49aca
Ömer Nalçacı <nalcaciomer@outlook.com>
2021-11-22 22:01:37 +0300

    22.11.2021 - 22:00

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,20 +1,41 @@
+import { users } from "../data/users.js"
+
 export default class UserService{
     constructor(loggerService){
-        this.users = []
+        this.employees = []
+        this.customers = []
+        this.errors = []
         this.loggerService = loggerService
     }
 
+    load(){
+        for (const user of users) {
+            switch (user.type) {
+                case "customer":
+                    this.customers.push(user)
+                    break;
+                
+                case "employee":
+                    this.employees.push(user)
+                    break;
+                default:
+                    this.errors.push(user)
+                    break;
+            }
+        }
+    }
+
     add(user){
-        this.users.push(user)
+        //this.users.push(user)
         this.loggerService.log(user)
     }
 
     list(){
-        return this.users
+        //return this.users
     }
 
     getById(id){
-        return this.users.find(u=> u.id === id)
+        //return this.users.find(u=> u.id === id)
     }
 
 }
